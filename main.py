fe5135fa201d3c13cd0c2d64db292cf155babe07
ljxmcm <ljxmcm@126.com>
2019-03-07 09:10:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,23 +7,23 @@ import os
 # The dominant components of each dataset are different
 # A larger value is assigned to topology parameter C_a  if graph structure is more informative than label information.
 # Here are the values we empirically found to work well for different datasets:
-#cora        C_a = 0.34, C_x = node_size/3,   alpha = 0.1, Micro-f1 = 0.8786
-#citeseer  C_a = 0.4,   C_x = node_size/20, alpha = 0.3, Micro-f1 = 0.7608
-#wiki         C_a = 0.5,   C_x = node_size/8,   alpha = 0.05, Micro-f1 = 0.802
+#cora        C_a = 0.34,  C_x = node_size/3,   alpha = 0.1
+#citeseer    C_a = 0.4,   C_x = node_size/20,  alpha = 0.3
+#wiki        C_a = 0.5,   C_x = node_size/8,   alpha = 0.05
 
-os.environ['CUDA_VISIBLE_DEVICES'] = "6"       
-
+#os.environ['CUDA_VISIBLE_DEVICES'] = "6"       
+C_a = 0.34
+C_x = R_Model.node_size/3
+alpha = 0.1
 R_Model = R_Model(edgelist='cora/cora.edgelist', features='cora/cora.features')
 
 #ranking
 R_x = R_Model.rank_X()
-R_A = R_Model.rank_A(alpha=0.05)
+R_A = R_Model.rank_A(alpha=alpha)
 #mapping
-C_a = 0.5
-C_x = R_Model.node_size/8
 T = mapping(vector=R_x, a=1, b=0, c=C_x)*mapping(vector=R_A, a=1, b=1, c=C_a)
 T = normalize(T)
-#train
+#training
 RANE = T_Model(T)
                    
 #evaluate
